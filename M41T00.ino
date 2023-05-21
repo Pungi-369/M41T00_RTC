f675d418cbfa5bf58bcd235ca1cc2d4f075a5a8c
Pungor Ferenc <60504091+Pungi-369@users.noreply.github.com>
2023-05-21 18:34:48 +0200

    Update M41T00.ino
    If you read the clock variables, use the master branch.
    - It contains the reading function.
    
    If you set the clock, you use the write-functions branch.
    - It contains the writing functon.

--- M41T00.ino
+++ M41T00.ino
@@ -1,165 +1,80 @@
 //////////////////////////////////////////////
 // What's the time?                         //
-//                                          //
+// Let's set!                               //
 // This is an old and easy code but useful. //
 // Created by Pungi                         //
 //////////////////////////////////////////////
 
-// Include some libs.. 
+// Include the wire lib 
 #include "Wire.h"
-#include <LiquidCrystal.h>
 
 // define the address of the RTC chip
 #define M41T00_I2C_ADDRESS 0x68
 
-// The arduino pin numbers and the LCD pins
-// LCD pins: 12 = RS, 11 = E, 5 = D4 , 4 = D5 , 3 = D6 , 2 = D7
-LiquidCrystal lcd(12, 11, 5, 4, 3, 2);      // LCD pin connections and it's initializing
-
-// HD44780 LCD special character
-// Let's create the byte array of the letter ó
-byte hosszuO[8] = {
-  0b00010,
-  0b00100,
-  0b01110,
-  0b10001,
-  0b10001,
-  0b10001,
-  0b01110,
-  0b00000,
-};
-
-// HD44780 LCD special character
-// Let's create the byte array of the letter é
-byte hosszuE[8] = {
-  0b00010,
-  0b00100,
-  0b01110,
-  0b10001,
-  0b11111,
-  0b10000,
-  0b01110,
-  0b00000
-};
-
-// Convert binary coded decimal to normal decimal numbers
-byte bcdToDec(byte val)
+// Convert normal decimal numbers to binary coded decimal
+byte decToBcd(byte val)
 {
-  return ( (val/16*10) + (val%16) );
+  return ( (val/10*16) + (val%10) );
 }
 
-// Gets the values from the M41T00
-void get_M41T00(byte *second, byte *minute, byte *hour, byte *dayOfWeek, byte *dayOfMonth, byte *month, byte *year)
+// Stops the M41T00, but it has the side effect of setting seconds to 0
+// Probably only want to use this for testing
+/*
+void stopM41T00()
 {
-  // Reset the register pointer
   Wire.beginTransmission(M41T00_I2C_ADDRESS);
   Wire.write(0);
+  Wire.write(0x80);
   Wire.endTransmission();
-  
-  Wire.requestFrom(M41T00_I2C_ADDRESS, 7);
+}
+*/
 
-  // A few of these need masks because certain bits are control bits
-  *second     = bcdToDec(Wire.read() & 0x7f);
-  *minute     = bcdToDec(Wire.read());
-  *hour       = bcdToDec(Wire.read() & 0x3f);  // Need to change this if 12 hour am/pm
-  *dayOfWeek  = bcdToDec(Wire.read());
-  *dayOfMonth = bcdToDec(Wire.read());
-  *month      = bcdToDec(Wire.read());
-  *year       = bcdToDec(Wire.read());
+// 1) Sets the date and time on the M41T00
+// 2) Starts the clock
+// 3) Sets hour mode to 24 hour clock
+// Assumes you're passing in valid numbers
+// second        // 0-59
+// minute        // 0-59
+// hour          // 1-23
+// dayOfWeek     // 1-7
+// dayOfMonth    // 1-28/29/30/31
+// month         // 1-12
+// year          // 0-99
+void set_M41T00(byte second, byte minute, byte hour, byte dayOfWeek, byte dayOfMonth, byte month, byte year)
+{
+   Wire.beginTransmission(M41T00_I2C_ADDRESS);
+   Wire.write(0);
+   Wire.write(decToBcd(second));    // 0 to bit 7 starts the clock
+   Wire.write(decToBcd(minute));
+   Wire.write(decToBcd(hour));      // If you want 12 hour am/pm you need to set
+                                   // bit 6 (also need to change readDateM41T00)
+   Wire.write(decToBcd(dayOfWeek));
+   Wire.write(decToBcd(dayOfMonth));
+   Wire.write(decToBcd(month));
+   Wire.write(decToBcd(year));
+   Wire.endTransmission();
 }
 
 void setup()
 {
+  // Create values
   byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
+  // Init the wire
   Wire.begin();
-  Serial.begin(9600);
-  lcd.begin(20, 4);
-  lcd.createChar(0, hosszuO);
-  lcd.createChar(1, hosszuE);
+  // Change these values to what you want to set your clock to.
+  // You probably only want to set your clock once and then remove
+  // the set_M41T00 call.
+  second = 30;
+  minute = 41;
+  hour = 18;
+  dayOfWeek = 3;
+  dayOfMonth = 23;
+  month = 4;
+  year = 14;
+  // Send the values to the chip
+  set_M41T00(second, minute, hour, dayOfWeek, dayOfMonth, month, year);
 }
 
 void loop()
 {
-  // Create byte values
-  byte second, minute, hour, dayOfWeek, dayOfMonth, month, year;
-  // Read the values of the I2C clock chip
-  get_M41T00(&second, &minute, &hour, &dayOfWeek, &dayOfMonth, &month, &year);
-  // Set the cursor. First position, first line.
-  lcd.setCursor(0, 0);
-  // Write the text. "Óra"
-  lcd.write(byte(0));
-  lcd.print("ra!");
-  // Set the cursor. Sixth position, second line.
-  lcd.setCursor(6, 1);
-  // If the number is less 10, write one space.
-  if(!(hour>9))
-  {
-    lcd.print(" ");
-  };
-  lcd.print(hour, DEC);
-  lcd.print(":");
-  // Set the cursor. Ninth position, second line.
-  lcd.setCursor(9, 1);
-  //  If the number is less 10, write one space.
-  if(!(minute>9))
-  {
-    lcd.print(" ");
-  };
-  lcd.print(minute, DEC);
-  lcd.print(":");
-  // Set the cursor. Twelfth position, second line.
-  lcd.setCursor(12, 1);
-  // If the number is less 10, write one space.
-  if (second<10)
-  {
-    lcd.print("0");
-  }; 
-  lcd.print(second, DEC);
-  // Set the cursor. First position, third line.
-  lcd.setCursor(0, 2);
-  lcd.print(2000+year, DEC);
-  lcd.print(".");
-  //  If the number is less 10, write one space.
-  if(!(month>9))
-  {
-    lcd.print(" ");
-  };
-  lcd.print(month, DEC);
-  lcd.print(".");
-  //  If the number is less 10, write one space.
-  if(!(dayOfMonth>9))
-  {
-    lcd.print(" ");
-  };
-  lcd.print(dayOfMonth, DEC);
-  // Set the cursor. First position, third line.
-  lcd.setCursor(0, 3);
-  // Write the text. "Hét napja: "
-  lcd.print("H");
-  lcd.write(byte(1));
-  lcd.print("t napja: ");
-  lcd.print(dayOfWeek, DEC);
-
-  // Serial sending
-  Serial.print(hour, DEC);
-  Serial.print(":");
-  Serial.print(minute, DEC);
-  Serial.print(":");
-  Serial.print(second, DEC);
-  Serial.print("  ");
-  Serial.print(month, DEC);
-  Serial.print("/");
-  Serial.print(dayOfMonth, DEC);
-  Serial.print("/");
-  Serial.print(year, DEC);
-  // Send "day of week" in hungarian. "Hét napja"
-  Serial.print("  H");
-  Serial.write(0xE9); //  é
-  Serial.print("t napja:");
-  Serial.println(dayOfWeek, DEC);
-  // Write my nick name.
-  Serial.print("Pungi");
-  Serial.write(0xAE);
-  Serial.println("");
-  delay(1000);
 }
